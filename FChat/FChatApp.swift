8c1c995defc9d45e587f2507b6602500f02f4f9e
ketto26 <86974154+ketto26@users.noreply.github.com>
2025-09-01 15:29:47 +0400

    Feature: Add beginning project files

---
+++ FChat/FChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FChatApp.swift
+//  FChat
+//
+//  Created by Keto Nioradze on 01.09.25.
+//
+
+import SwiftUI
+
+@main
+struct FChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeScreenView()
+        }
+    }
+}
---
+++ FChat/FChatHomeScreen/HomeScreenView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeScreenView.swift
+//  FChat
+//
+//  Created by Keto Nioradze on 01.09.25.
+//
+
+import SwiftUI
+
+struct HomeScreenView: View {
+    var body: some View {
+        VStack{
+            Text("FChat")
+                .font(.largeTitle)
+            
+            Text("Chatting Main Screen")
+        }
+    }
+}
+
+#Preview {
+    HomeScreenView()
+}
